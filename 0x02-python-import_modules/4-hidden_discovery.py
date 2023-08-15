287e801dadbce8069ad2cc3defe4889e258a7c34
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-15 15:17:48 +0300

    task 5

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,7 @@
 if __name__ == "__main__":
      import hidden_4
 
-    names = dir(hidden_4)
-    for name in names:
+    allNames = dir(hidden_4)
+    for name in allNames:
         if name[0:2] != "__":
             print(name)
