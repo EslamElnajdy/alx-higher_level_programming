e8e2b66f3826d066970a938243b650ef7249d060
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-16 14:42:37 +0300

    fix error

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -4,4 +4,5 @@ def new_in_list(my_list, idx, element):
     a = []
     if idx >= 0 and idx < len(my_list):
         a = my_list[:idx] + [element] + my_list[idx + 1:]
-    return (a)
+        return (a)
+    return (my_list)
