16951cef5af09f1d612587b624d788528775db8f
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-20 05:32:29 +0300

    task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -2,4 +2,8 @@
 def best_score(a_dictionary):
     if not a_dictionary:
         return None
-    return max(a_dictionary)
+    value = (0, "")
+    for key in a_dictionary:
+        if a_dictionary[key] > value[0]:
+            value = (a_dictionary[key], key)
+    return value[1]
