12b29a7361d0cadd12948699da89178e5349c657
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-21 15:14:22 +0300

    fix error

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,9 +4,9 @@ def roman_to_int(roman_string):
         return 0
     roman_di = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
     num = 0
-    for i in roman_string:
-        if roman_di[i] > num:
-            num = roman_di[i] - num
+    for i in range(len(roman_string)):
+        if i > 0 and roman_di[roman_string[i]] > roman_di[roman_string[i - 1]]:
+            num += roman_di[roman_string[i]] - 2 * roman_di[roman_string[i - 1]]
         else:
-            num += roman_di[i]
+            num += roman_di[roman_string[i]]
     return num
