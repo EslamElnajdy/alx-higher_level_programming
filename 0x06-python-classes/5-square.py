f5fc55f5172c041d6c9272bb40da732e4f6db3c4
Eslam Mohamed <eslammhamed425@gmail.com>
2023-08-30 04:53:55 +0300

    task 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+""" Square module """
+
+
+class Square:
+    """ body of  a square class """
+
+    def __init__(self, size=0) -> None:
+        """
+        Initializes class attributes
+
+        Args:
+            size: size of square
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        """
+        get the attribute
+        """
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """
+        calculate the area of a square
+        """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ rints in stdout the square with the character # """
+
+        for i in range(self.size):
+            print("#" * self.size)
