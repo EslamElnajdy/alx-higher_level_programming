aa1498b818446e5ffe9f3184881a324f75c4101d
Eslam Mohamed <eslammhamed425@gmail.com>
2023-09-03 02:45:44 +0300

    task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Module add-integer
+adds two integer
+"""
+def add_integer(a, b=98):
+    """return the addition of a and b"""
+    if type(a) is not int and type(a) is not float:
+        raise TypeError("a must be an integer")
+    elif type(b) is not int and type(b) is not float:
+        raise TypeError("b must be an integer")
+    else:
+        return int(a) + int(b)
