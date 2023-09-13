e70fa41970b801569e939e7e519c3abb432dc604
Eslam Mohamed <eslammhamed425@gmail.com>
2023-09-13 06:08:42 +0300

    task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""module doc"""
+import sys
+import os
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+filename = "add_item.json"
+if os.path.isfile(filename):
+    f_list = load_from_json_file(filename)
+else:
+    f_list = []
+for i in range(1, len(sys.argv)):
+    f_list.append(sys.argv[i])
+save_to_json_file(f_list, filename)
