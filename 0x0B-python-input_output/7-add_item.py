74bdca1ed3f75fce9e605fe340abc524f7de0253
Eslam Mohamed <eslammhamed425@gmail.com>
2023-09-14 04:08:11 +0300

    task 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """module doc"""
-import sys
-import os
+from sys import argv
+from os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
@@ -11,11 +11,11 @@ def add_items():
     func doc
     """
     filename = "add_item.json"
-    if os.path.isfile(filename):
+    if path.isfile(filename):
         f_list = load_from_json_file(filename)
     else:
         f_list = []
-    for i in range(1, len(sys.argv)):
+    for i in range(1, len(argv)):
         f_list.append(sys.argv[i])
     save_to_json_file(f_list, filename)
 
