cbcfded3e1f9bfa1ef4e2816c3c897e556172175
Eslam Mohamed <eslammhamed425@gmail.com>
2023-11-15 22:01:08 +0200

    task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-const { argv } = require('node:process');
-if (argv.length === 2) {
+const argsCount = process.argv.length;
+if (argsCount === 2) {
   console.log('No argument');
-} else if (argv.length === 3) {
+} else if (argsCount === 3) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
