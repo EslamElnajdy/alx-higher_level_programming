be08a4d60d1b9fe5dddbe1949573597b9bf49bbd
Eslam Mohamed <eslammhamed425@gmail.com>
2023-11-16 22:46:38 +0200

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,10 +1,6 @@
 #!/usr/bin/node
 const square = require('./5-square');
 class Square extends square {
-  constructor (size) {
-    super(size);
-  }
-
   charPrint (c = 'X') {
     for (let i = 0; i < this.height; i++) {
       console.log(c.repeat(this.width));
