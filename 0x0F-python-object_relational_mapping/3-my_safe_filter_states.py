30427bf48834020feffbd39f666f07450f17032c
Eslam <114030950+EslamElnajdy@users.noreply.github.com>
2023-11-20 17:14:26 +0000

    update

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
         charset="utf8"
     )
     cursor = db.cursor()
-    query = "SELECT * FROM states WHERE name LIKE BINARY %s\
+    query = "SELECT * FROM states WHERE name LIKE %s\
              ORDER BY states.id;".format(argv[4])
     cursor.execute(query, (argv[4],))
 
