5b15c3c5ccd3fb5a3b242d720863b32d6ba5bede
Eslam Mohamed <eslammhamed425@gmail.com>
2023-12-12 02:30:17 +0200

    fix

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 # displays the body of the response
-if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" == '200' ]; then
-  curl -sL "$1"
-fi
+if [ "$(curl -s -o /dev/null -w "%{http_code}" "$1")" == '200' ]; then curl -sL "$1"; fi
