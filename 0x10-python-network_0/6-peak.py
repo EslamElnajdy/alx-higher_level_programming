7f897ea5d68d653a95f5362477170e567394778c
Eslam Mohamed <eslammhamed425@gmail.com>
2023-12-12 02:56:58 +0200

    task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""script to return the peak value
+"""
+
+
+def find_peak(list_of_integers):
+    """ return the peak value"""
+    if not list_of_integers:
+        return None
+
+    sorted_list = sorted(list_of_integers)
+    return sorted_list[-1]
+    
