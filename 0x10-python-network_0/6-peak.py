cb99e170d3fd31b1470f216a43246a1111b3473e
Eslam Mohamed <eslammhamed425@gmail.com>
2023-12-12 03:19:16 +0200

    fix time complexity

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -5,8 +5,17 @@
 
 def find_peak(list_of_integers):
     """ return the peak value"""
-    peak = None
-    for ele in list_of_integers:
-        if peak is None or peak < ele:
-            peak = ele
-    return peak
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            high = mid
+        else:
+            low = mid + 1
+
+    return list_of_integers[low]
