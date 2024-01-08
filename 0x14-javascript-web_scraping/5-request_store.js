2d8edc0b6a073c5f589e41a2fddff16dd95b5e45
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-08 04:17:33 +0200

    task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -9,8 +9,11 @@ const filename = process.argv[3];
 request(url, (err, res, body) => {
   if (err) {
     console.error(err);
+    return;
   }
   fs.writeFile(filename, body, 'utf8', (error) => {
-    console.log(error);
+    if (error) {
+      console.log(error);
+    }
   });
 });
