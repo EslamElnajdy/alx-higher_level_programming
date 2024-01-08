bd33cf4f5440890f8a7d544ea9630ea619d6e0d9
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-08 11:02:00 +0200

    task 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -8,14 +8,23 @@ request(apiUrl, (err, res, body) => {
     console.log(err);
   } else {
     const data = JSON.parse(body).characters;
-    for (let i = 0; i < data.length; i++) {
-      request(data[i], (err, res, body2) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(JSON.parse(body2).name);
-        }
-      });
-    }
+    const promises = data.map(character => {
+        return new Promise((resolve, reject) => {
+            request(character, (err, res, body2) => {
+                if (err) {
+                reject(err);
+                } else {
+                resolve(JSON.parse(body2).name);
+                }
+            });
+        });
+    })
+    Promise.all(promises)
+    .then(names => {
+        names.forEach(name => console.log(name));
+    })
+    .catch(error => {
+        console.error(error);
+    })
   }
 });
