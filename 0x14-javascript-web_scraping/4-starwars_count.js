42a08f97be681eda8a96573037a4aadc2273cb6d
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-08 03:51:18 +0200

    task 4

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -3,14 +3,20 @@
 const request = require('request');
 
 const apiUrl = process.argv[2];
-const characterId = 18;
 
 request(apiUrl, (error, res, body) => {
   if (error) {
     console.error(error);
   } else if (res.statusCode === 200) {
     const films = JSON.parse(body).results;
-    const count = films.filter(film => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`)).length;
+    let count = 0;
+    for (let i = 0; i < films.length; i++) {
+      for (let j = 0; j < films[i].characters.length; j++) {
+        if (films[i].characters[j].includes('/18/')) {
+          count++;
+        }
+      }
+    }
     console.log(count);
   }
 });
