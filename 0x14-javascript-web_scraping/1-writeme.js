8ef1aae75dc3c247d0a423fe65f33300d5068122
Eslam Mohamed <eslammhamed425@gmail.com>
2024-01-08 02:53:23 +0200

    task 1

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -3,6 +3,6 @@
 import { writeFile } from 'fs';
 const data = process.argv[1];
 const filename = process.argv[2];
-writeFile(data, filename, 'utf-8', (err) => {
+writeFile(filename, data, 'utf-8', (err) => {
   if (err) throw err;
 });
